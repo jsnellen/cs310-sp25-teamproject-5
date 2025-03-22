20ad489d0a72669e16952ac692cf56415931c7ef
Ralph Wheeler <rwheeler@stu.jsu.edu>
2025-03-22 09:57:32 -0500

    Absenteeism class. Sorry I forgot to commit it.

---
+++ src/edu/jsu/mcis/cs310/tas_sp25/Absenteeism.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.jsu.mcis.cs310.tas_sp25;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+
+/**
+ *
+ * @author rwhee
+ */
+public class Absenteeism {
+    private Employee employeeid;
+    private LocalDate startDate;
+    private BigDecimal percentage;
+    
+    public Absenteeism(HashMap<Object, Object> AbsenteeDetail){
+        this.employeeid = (Employee) AbsenteeDetail.get("employeeid");
+        this.startDate = (LocalDate) AbsenteeDetail.get("startDate");
+        this.percentage = (BigDecimal) AbsenteeDetail.get("percentage");
+    }
+
+    public Employee getEmployeeid() {
+        return employeeid;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public BigDecimal getPercentage() {
+        return percentage;
+    }
+    
+    @Override
+    public String toString() {
+        StringBuilder build = new StringBuilder();
+        DateTimeFormatter start = DateTimeFormatter.ofPattern("MM-dd-yyyy");
+        
+        // Append Absenteeism details to the StringBuilder
+        build.append("#")
+                .append(employeeid.getId()).append(" ")
+                .append("Pay Period Starting ")
+                .append(startDate.format(start))
+                .append(": ")
+                .append(percentage.toString())
+                .append("%");
+
+
+        return build.toString();
+
+        }
+    
+}
